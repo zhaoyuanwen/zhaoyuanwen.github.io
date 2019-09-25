4e802f7af3f5c81d18186de32d0281c45b392900
zyw <zyw@163.com>
2019-09-25 08:57:40 +0800

    init project

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,51 @@
 let gulp = require("gulp");
 
 let app = {
-    src : "./src",
-    dist : "./dist",
-    bak : "./bak"
+    fonts : "./node_modules/bootstrap/fonts",
+    js : "./node_modules/bootstrap/js",
+    less : "./node_modules/bootstrap/less",
+    dist : "./node_modules/bootstrap/dist"
 }
 
-//html压缩
-let htmlmin = require("gulp-htmlmin");
-
-gulp.task("htmlmin",function(done){
-    gulp.src(`${app.src}/**/*.{htm,html}`)
-        .pipe(htmlmin({
-            removeComments: true,
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-        }))
+//复制fonts
+gulp.task("copy",function(done){
+    gulp.src(app.fonts)
         .pipe(gulp.dest(app.dist));
+    gulp.src(app.fonts + "/**")
+        .pipe(gulp.dest(app.dist + "/fonts"));    
     done();
 });
 
-//css压缩
-let cssmin = require("gulp-cssmin");
+//整合js为bootstrap.js
 
-gulp.task("cssmin",function(done){
-    gulp.src(`${app.src}/**/*.css`)
-        .pipe(cssmin())
-        .pipe(gulp.dest(app.dist));
-    done();
-});
-
-//js模糊混淆
 let jsmin = require("gulp-uglify-es");
-
-gulp.task("jsmin",function(done){
-    gulp.src(`${app.src}/**/*.js`)
-        .pipe(jsmin.default())
-        .pipe(gulp.dest(app.dist));
-    done();
-});
-
-//重命名压缩
-let rename = require("gulp-rename");
-
-gulp.task("bak",function(done){
-    gulp.src(`${app.src}/**`)
-        .pipe(rename(function(target){
-            // console.log(target);
-            if(target.extname){
-                target.extname += ".bak";
-            }
-        }))
-        .pipe(gulp.dest(app.bak));
-    done();
-});
-
-//名字拼接压缩
 let concat = require("gulp-concat");
 
 gulp.task("concat",function(done){
-    gulp.src(`${app.src}/**/*.css`)
-        .pipe(concat("all.css"))
+    gulp.src(app.js)
+        .pipe(gulp.dest(app.dist));
+    gulp.src(`${app.js}/*.js`)
+        .pipe(concat("bootstrap.js"))
+        .pipe(jsmin.default())
+        .pipe(gulp.dest(app.dist + "/js"));
+    done();
+});
+
+//编译less文件并整合成bootstrap.css
+let rename = require("gulp-rename");
+let less = require("gulp-less");
+let cssmin = require("gulp-cssmin");
+
+gulp.task("less",function(done){
+    // gulp.src(app.less)
+    //     .pipe(rename("css"))
+    //     .pipe(gulp.dest(app.dist));
+    gulp.src(`${app.less}/*.less`)
+        .pipe(less())
+        .pipe(concat("bootstrap.css"))
         .pipe(cssmin())
+        .pipe(rename("bootstrap.min.css"))
         .pipe(gulp.dest(app.dist));
     done();
 });
 
-//整合less文件
-let less = require("gulp-less");
-
-gulp.task("less1",function(done){
-    gulp.src(`${app.src}/less/*.less`)
-        .pipe(less())
-        .pipe(concat("all.css"))
-        .pipe(gulp.dest(app.dist + "/css"));
-    done();
-});
-
-gulp.task("less2",function(done){
-    gulp.src(`${app.src}/less/*.less`)
-        .pipe(less())
-        .pipe(concat("all.css"))
-        .pipe(cssmin())
-        .pipe(rename("all.min.css"))
-        .pipe(gulp.dest(app.dist + "/css"));
-    done();
-});
